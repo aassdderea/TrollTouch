ca198263a9b9751af5b7f93afa79eb85b25dc17f
luofeng123123 <38125652+luofeng123123@users.noreply.github.com>
2026-07-01 01:33:58 +0800

    Update TrollTouch.m

--- TrollTouch.m
+++ TrollTouch.m
@@ -25,6 +25,8 @@ static NSArray *_skipKeywords;
 static NSString *_currentMode;
 static UILabel *_statusLabel;
 static UIWindow *_statusWindow;
+static UIWindow *_panelWindow;
+static UITextField *_pxField, *_pyField;
 
 static void TTUpdateStatusLabel(NSString *text, UIColor *color) {
     dispatch_async(dispatch_get_main_queue(), ^{
@@ -77,6 +79,95 @@ static void TTShowStatusLabel(void) {
         TTLog(@"独立状态窗口已创建");
     });
 }
+
+@interface TTPanelHelper : NSObject @end
+@implementation TTPanelHelper
++ (void)doTap { TTDoTapFromPanel(); }
+@end
+
+static void TTDoTapFromPanel(void) {
+    CGFloat x = [_pxField.text floatValue];
+    CGFloat y = [_pyField.text floatValue];
+    if (x <= 0) x = 200; if (y <= 0) y = 400;
+    TTTapHIDAtPoint(CGPointMake(x, y));
+    TTLog(@"面板触发 HID (%.0f,%.0f)", x, y);
+}
+
+static void TTShowControlPanel(void) {
+    if (_panelWindow) return;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIWindowScene *scene = nil;
+        if (@available(iOS 13.0, *)) {
+            for (UIScene *c in UIApplication.sharedApplication.connectedScenes) {
+                if ([c isKindOfClass:[UIWindowScene class]] && c.activationState == UISceneActivationStateForegroundActive) {
+                    scene = (UIWindowScene *)c; break;
+                }
+            }
+            if (!scene) {
+                for (UIScene *c in UIApplication.sharedApplication.connectedScenes) {
+                    if ([c isKindOfClass:[UIWindowScene class]]) { scene = (UIWindowScene *)c; break; }
+                }
+            }
+        }
+        if (!scene) return;
+
+        CGFloat sw = UIScreen.mainScreen.bounds.size.width;
+        CGFloat pw = sw - 32.0;
+        CGFloat h = 60.0;
+        CGFloat y = UIScreen.mainScreen.bounds.size.height - h - 40.0;
+
+        _panelWindow = [[UIWindow alloc] initWithWindowScene:scene];
+        _panelWindow.frame = CGRectMake(16, y, pw, h);
+        _panelWindow.windowLevel = UIWindowLevelAlert + 99;
+        _panelWindow.backgroundColor = [UIColor colorWithWhite:0.08 alpha:0.92];
+        _panelWindow.layer.cornerRadius = 12.0;
+        _panelWindow.clipsToBounds = YES;
+
+        UIViewController *vc = [UIViewController new];
+        vc.view.backgroundColor = [UIColor clearColor];
+
+        UILabel *xl = [[UILabel alloc] initWithFrame:CGRectMake(10, 8, 16, 20)];
+        xl.text = @"X"; xl.textColor = UIColor.whiteColor; xl.font = [UIFont systemFontOfSize:13];
+        [vc.view addSubview:xl];
+
+        UITextField *xf = [[UITextField alloc] initWithFrame:CGRectMake(28, 8, 70, 32)];
+        xf.text = @"200"; xf.borderStyle = UITextBorderStyleRoundedRect;
+        xf.keyboardType = UIKeyboardTypeNumberPad;
+        xf.textColor = UIColor.whiteColor;
+        xf.backgroundColor = [UIColor colorWithWhite:0.2 alpha:1.0];
+        xf.font = [UIFont systemFontOfSize:14];
+        [vc.view addSubview:xf];
+        _pxField = xf;
+
+        UILabel *yl = [[UILabel alloc] initWithFrame:CGRectMake(106, 8, 16, 20)];
+        yl.text = @"Y"; yl.textColor = UIColor.whiteColor; yl.font = [UIFont systemFontOfSize:13];
+        [vc.view addSubview:yl];
+
+        UITextField *yf = [[UITextField alloc] initWithFrame:CGRectMake(124, 8, 70, 32)];
+        yf.text = @"400"; yf.borderStyle = UITextBorderStyleRoundedRect;
+        yf.keyboardType = UIKeyboardTypeNumberPad;
+        yf.textColor = UIColor.whiteColor;
+        yf.backgroundColor = [UIColor colorWithWhite:0.2 alpha:1.0];
+        yf.font = [UIFont systemFontOfSize:14];
+        [vc.view addSubview:yf];
+        _pyField = yf;
+
+        UIButton *tapBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+        tapBtn.frame = CGRectMake(204, 8, pw - 214, 32);
+        [tapBtn setTitle:@"TAP" forState:UIControlStateNormal];
+        [tapBtn setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
+        tapBtn.backgroundColor = [UIColor colorWithRed:0.15 green:0.55 blue:0.92 alpha:1.0];
+        tapBtn.layer.cornerRadius = 8;
+        tapBtn.titleLabel.font = [UIFont boldSystemFontOfSize:14];
+        [tapBtn addTarget:[TTPanelHelper class] action:@selector(doTap) forControlEvents:UIControlEventTouchUpInside];
+        [vc.view addSubview:tapBtn];
+
+        _panelWindow.rootViewController = vc;
+        _panelWindow.hidden = NO;
+        TTLog(@"控制面板已创建");
+    });
+}
+
     @try {
         [_logHandle writeData:[line dataUsingEncoding:NSUTF8StringEncoding]];
         [_logHandle synchronizeFile];
@@ -255,12 +346,18 @@ static void TTHandleCommand(void) {
     NSDictionary *cmd = [NSDictionary dictionaryWithContentsOfFile:kCommandPath];
     if (![cmd isKindOfClass:[NSDictionary class]]) return;
     NSString *mode = cmd[@"mode"];
-    if ([mode isEqualToString:@"auto"]) { TTStartAutoScan(); }
-    else if ([mode isEqualToString:@"tap"]) {
+    if ([mode isEqualToString:@"auto"]) {
+        TTStartAutoScan();
+        [@{} writeToFile:kCommandPath atomically:YES];
+    } else if ([mode isEqualToString:@"tap"]) {
         CGFloat x = [cmd[@"x"] isKindOfClass:[NSNumber class]] ? [cmd[@"x"] floatValue] : 0;
         CGFloat y = [cmd[@"y"] isKindOfClass:[NSNumber class]] ? [cmd[@"y"] floatValue] : 0;
+        [@{} writeToFile:kCommandPath atomically:YES];
         TTTapHIDAtPoint(CGPointMake(x, y));
-    } else if ([mode isEqualToString:@"stop"]) { TTStopScan(); }
+    } else if ([mode isEqualToString:@"stop"]) {
+        TTStopScan();
+        [@{} writeToFile:kCommandPath atomically:YES];
+    }
 }
 
 #pragma mark - init
@@ -293,6 +390,7 @@ __attribute__((constructor)) static void TrollTouchInit(void) {
     kCommandPath = [docs stringByAppendingPathComponent:@"com.trolltouch.command.plist"];
     kLogPath     = [docs stringByAppendingPathComponent:@"com.trolltouch.log.txt"];
 
+    // 始终轮询命令文件, 不需要 notify
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         _skipKeywords = @[@"跳过", @"关闭", @"知道了", @"好的", @"同意",
                           @"skip", @"close", @"Skip", @"Close",
@@ -300,18 +398,32 @@ __attribute__((constructor)) static void TrollTouchInit(void) {
                           @"立刻体验", @"立即体验", @"马上体验",
                           @"×", @"✕", @"X"];
 
-        TTLog(@"TrollTouch v2.1 loaded");
+        TTLog(@"TrollTouch v2.2 loaded");
         TTLog(@"bundle=%@", NSBundle.mainBundle.bundleIdentifier ?: @"?");
-        TTLog(@"logPath=%@", kLogPath);
+        TTLog(@"cmdPath=%@", kCommandPath);
 
         TTShowStatusLabel();
+        TTShowControlPanel();
 
+        // 轮询命令文件, 0.5秒间隔
+        [NSTimer scheduledTimerWithTimeInterval:0.5 repeats:YES block:^(NSTimer *t) {
+            TTHandleCommand();
+        }];
+
+        // 注册 notify 作为即时触发
         int token = 0;
-        notify_register_dispatch(kNotifyName.UTF8String, &token, dispatch_get_main_queue(), ^(int t) {
-            (void)t; TTHandleCommand();
+        notify_register_dispatch(kNotifyName.UTF8String, &token, dispatch_get_main_queue(), ^(int t2) {
+            (void)t2; TTHandleCommand();
         });
 
+        // 自动扫描
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            if (!_scanTimer && ![_currentMode isEqualToString:@"auto"]) {
+                TTStartAutoScan();
+            }
+        });
+    });
+}
             if (!_scanTimer && ![_currentMode isEqualToString:@"auto"]) {
                 TTStartAutoScan();
                 TTLog(@"自动扫描已启动");
